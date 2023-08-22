8cfdbc8c9f78911080b6dbbff29519c43dcd1ba3
woksin <sindrewilting@gmail.com>
2023-08-22 12:32:26 +0200

    Introduce MSTest specifications abstractions

---
+++ Packages/DotNET/Specifications/Tests/MSTest.Tests/for_Specification/when_specification_is_not_derived/when_test_has_asyncronous_arange_and_act.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) woksin-org. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Woksin.Extensions.Specifications.MSTest.for_Specification.when_specification_is_not_derived;
+
+[TestClass]
+public class when_test_has_asyncronous_arange_and_act : Specification
+{
+    int some_dependency;
+    bool result;
+
+    async Task Establish()
+	{
+		await Task.Delay(100);
+		some_dependency = 32;
+	}
+
+    async Task Because()
+	{
+		await Task.Delay(100);
+		result = true;
+	}
+
+	[TestMethod]
+	public void should_do_arrange_part() => Assert.AreEqual(32, some_dependency);
+	
+	[TestMethod]
+	public void should_do_act_part() => Assert.IsTrue(result);
+}
---
+++ Packages/DotNET/Specifications/Tests/MSTest.Tests/for_Specification/when_specification_is_not_derived/when_test_has_syncronous_arange_and_act.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) woksin-org. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Woksin.Extensions.Specifications.MSTest.for_Specification.when_specification_is_not_derived;
+
+[TestClass]
+public class when_test_has_syncronous_arange_and_act : Specification
+{
+    int some_dependency;
+    bool result;
+
+    void Establish() => some_dependency = 32;
+
+    void Because() => result = true;
+
+	[TestMethod]
+	public void should_do_arrange_part() => Assert.AreEqual(32, some_dependency);
+	
+	[TestMethod]
+	public void should_do_act_part() => Assert.IsTrue(result);
+}
