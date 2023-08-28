b889cbb31d23f621736d11ccb07371580817ad9c
github-action-benchmark <github@users.noreply.github.com>
2023-08-28 21:53:05 +0000

    add Woksin.Extensions.IoC Benchmarks (benchmarkdotnet) benchmark result for 65ccc127508569469bea09fe17b6bdb8dfdc6f4e

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1693259585856,
+  "repoUrl": "https://github.com/woksin-org/Woksin.Extensions",
+  "entries": {
+    "Woksin.Extensions.IoC Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "sindrewilting@gmail.com",
+            "name": "woksin",
+            "username": "woksin"
+          },
+          "committer": {
+            "email": "sindrewilting@gmail.com",
+            "name": "woksin",
+            "username": "woksin"
+          },
+          "distinct": true,
+          "id": "65ccc127508569469bea09fe17b6bdb8dfdc6f4e",
+          "message": "Add benchmarks",
+          "timestamp": "2023-08-28T23:51:16+02:00",
+          "tree_id": "777fd7d21840490785c03f5f1458cec6b36f5512",
+          "url": "https://github.com/woksin-org/Woksin.Extensions/commit/65ccc127508569469bea09fe17b6bdb8dfdc6f4e"
+        },
+        "date": 1693259584757,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Woksin.Extensions.IoC.Benchmarks.BuildHost.MicrosoftIoC",
+            "value": 7537686.823529412,
+            "unit": "ns",
+            "range": "± 153099.4963901071"
+          },
+          {
+            "name": "Woksin.Extensions.IoC.Benchmarks.BuildHost.AutofacIoC",
+            "value": 35435902.69,
+            "unit": "ns",
+            "range": "± 5769209.238335781"
+          }
+        ]
+      }
+    ]
+  }
+}
