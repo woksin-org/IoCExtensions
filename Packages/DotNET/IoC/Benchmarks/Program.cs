fd82972eacb2d2d71ce948a7818f5f6e87b56693
woksin <sindrewilting@gmail.com>
2023-08-29 01:00:23 +0200

    Config for benchmark

--- Packages/DotNET/IoC/Benchmarks/Program.cs
+++ Packages/DotNET/IoC/Benchmarks/Program.cs
@@ -1,7 +1,11 @@
 // Copyright (c) woksin-org. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
+using Perfolizer.Horology;
 
 namespace Woksin.Extensions.IoC.Benchmarks;
 
@@ -9,6 +13,12 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        var summary = BenchmarkRunner.Run(typeof(Program).Assembly, new ManualConfig()
+        {
+            SummaryStyle = SummaryStyle.Default
+                .WithRatioStyle(RatioStyle.Percentage)
+                .WithSizeUnit(SizeUnit.KB)
+                .WithTimeUnit(TimeUnit.Millisecond),
+        }, args);
     }
 }
