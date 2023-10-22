f51b04f8848d5a369054f67a3bcf0c644607c804
woksin <sindrewilting@gmail.com>
2023-10-22 22:30:10 +0200

    Special Root Container options factory

--- Packages/DotNET/Configurations/Source/Tenancy/ConfigurationOptionsServiceAdder.cs
+++ Packages/DotNET/Configurations/Source/Tenancy/ConfigurationOptionsServiceAdder.cs
@@ -1,6 +1,7 @@
 // Copyright (c) woksin-org. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Woksin.Extensions.IoC;
 using Woksin.Extensions.IoC.Tenancy;
@@ -10,18 +11,32 @@ namespace Woksin.Extensions.Configurations.Tenancy;
 /// <summary>
 /// Represents an implementation of <see cref="ICanAddServicesForTypesWith{TAttribute,TContainerBuilder}"/> for <see cref="ConfigurationAttribute"/>.
 /// </summary>
+[DisableAutoRegistration]
 public class ConfigurationOptionsServiceAdder : ICanAddServicesForTypesWith<ConfigurationAttribute>
 {
     /// <inheritdoc />
     public void AddServiceFor(Type type, ConfigurationAttribute attribute, IServiceCollection services)
+        => AddConfigurationObjectDefinition(services, type, attribute.ConfigurationPath, type.IsPerTenant());
+
+    static void AddConfigurationObjectDefinition(IServiceCollection services, Type type, string configurationPath, bool perTenant)
     {
-        if (Attribute.IsDefined(type, typeof(PerTenantAttribute)))
+        if (perTenant)
         {
-            services.AddTenantConfigurationObjectDefinitionFor(type, attribute.ConfigurationPath);
+            services.AddTenantScopedServices((tenantId, tenantServices) => AddConfigurationObjectDefinitionTypes(
+                tenantServices,
+                typeof(ConfigurationObjectDefinition<>).MakeGenericType(type),
+                ConfigurationPath.Combine(tenantId, configurationPath)));
         }
         else
         {
-            services.AddConfigurationObjectDefinitionFor(type, attribute.ConfigurationPath);
+            AddConfigurationObjectDefinitionTypes(services, typeof(ConfigurationObjectDefinition<>).MakeGenericType(type), configurationPath);
         }
+    }
+
+    static void AddConfigurationObjectDefinitionTypes(IServiceCollection services, Type definitionType, string configurationPath)
+    {
+        var definition = Activator.CreateInstance(definitionType, configurationPath)!;
+        services.AddSingleton(definitionType, definition);
+        services.AddSingleton(typeof(IAmAConfigurationObjectDefinition), provider => provider.GetRequiredService(definitionType));
     }
 }
--- Packages/DotNET/Configurations/Source/Tenancy/HostExtensions.cs
+++ Packages/DotNET/Configurations/Source/Tenancy/HostExtensions.cs
@@ -35,65 +35,10 @@ public static class HostExtensions
     {
         services.Configure<IoCSettings>(settings => settings.AdditionalAssemblies.Add(typeof(HostExtensions).Assembly));
 		AddConfigurationPrefix(services, configurationPrefixes);
-		services.Add(ServiceDescriptor.Singleton(typeof(IOptionsFactory<>), typeof(ConfigurationsExtensionOptionsFactory<>)));
+		services.Add(ServiceDescriptor.Singleton(typeof(IOptionsFactory<>), typeof(RootContainerConfigurationsExtensionOptionsFactory<>)));
         services.AddTenantScopedServices(tenantServices => tenantServices.Add(ServiceDescriptor.Singleton(typeof(IOptionsFactory<>), typeof(ConfigurationsExtensionOptionsFactory<>))));
         return services;
 	}
-
-    /// <summary>
-    /// Adds a configuration object definition.
-    /// </summary>
-    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
-    /// <param name="configurationPathParts">The configuration path parts of the configuration object.</param>
-    /// <typeparam name="TConfigurationType">The <see cref="Type"/> of the <see cref="ConfigurationObjectDefinition{TConfiguration}"/>.</typeparam>
-    public static void AddConfigurationObjectDefinitionFor<TConfigurationType>(this IServiceCollection services, params string[] configurationPathParts)
-        => services.AddConfigurationObjectDefinitionFor(typeof(TConfigurationType), configurationPathParts);
-
-    /// <summary>
-    /// Adds a configuration object definition.
-    /// </summary>
-    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
-    /// <param name="type">The <see cref="Type"/> of the <see cref="ConfigurationObjectDefinition{TConfiguration}"/>.</param>
-    /// <param name="configurationPathParts">The configuration path parts of the configuration object.</param>
-    public static void AddConfigurationObjectDefinitionFor(this IServiceCollection services, Type type, params string[] configurationPathParts)
-        => services.AddConfigurationObjectDefinitionFor(type, ConfigurationPath.Combine(configurationPathParts));
-
-    /// <summary>
-    /// Adds a configuration object definition.
-    /// </summary>
-    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
-    /// <param name="type">The <see cref="Type"/> of the <see cref="ConfigurationObjectDefinition{TConfiguration}"/>.</param>
-    /// <param name="configurationPath">The configuration path of the configuration object.</param>
-    public static void AddConfigurationObjectDefinitionFor(this IServiceCollection services, Type type, string configurationPath)
-        => AddConfigurationObjectDefinition(services, type, configurationPath, false);
-    
-    /// <summary>
-    /// Adds a tenant configuration object definition.
-    /// </summary>
-    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
-    /// <param name="configurationPathParts">The configuration path parts of the configuration object.</param>
-    /// <typeparam name="TConfigurationType">The <see cref="Type"/> of the <see cref="ConfigurationObjectDefinition{TConfiguration}"/>.</typeparam>
-    public static void AddTenantConfigurationObjectDefinitionFor<TConfigurationType>(this IServiceCollection services, params string[] configurationPathParts)
-        => services.AddTenantConfigurationObjectDefinitionFor(typeof(TConfigurationType), configurationPathParts);
-
-
-    /// <summary>
-    /// Adds a tenant configuration object definition.
-    /// </summary>
-    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
-    /// <param name="type">The <see cref="Type"/> of the <see cref="ConfigurationObjectDefinition{TConfiguration}"/>.</param>
-    /// <param name="configurationPathParts">The configuration path parts of the configuration object.</param>
-    public static void AddTenantConfigurationObjectDefinitionFor(this IServiceCollection services, Type type, params string[] configurationPathParts)
-        => services.AddConfigurationObjectDefinitionFor(type, ConfigurationPath.Combine(configurationPathParts));
-
-    /// <summary>
-    /// Adds a tenant configuration object definition.
-    /// </summary>
-    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
-    /// <param name="type">The <see cref="Type"/> of the <see cref="ConfigurationObjectDefinition{TConfiguration}"/>.</param>
-    /// <param name="configurationPath">The configuration path of the configuration object.</param>
-    public static void AddTenantConfigurationObjectDefinitionFor(this IServiceCollection services, Type type, string configurationPath)
-        => AddConfigurationObjectDefinition(services, type, configurationPath, true);
 
     static void AddConfigurationPrefix(IServiceCollection serviceCollection, string[] configurationPrefixes)
     {
@@ -104,27 +49,5 @@ public static class HostExtensions
         }
 
         serviceCollection.TryAddSingleton(new ConfigurationPrefix(prefix));
-    }
-
-    static void AddConfigurationObjectDefinition(IServiceCollection services, Type type, string configurationPath, bool perTenant)
-    {
-        if (perTenant)
-        {
-            services.AddTenantScopedServices((tenantId, tenantServices) => AddConfigurationObjectDefinitionTypes(
-                tenantServices,
-                typeof(ConfigurationObjectDefinition<>).MakeGenericType(type),
-                ConfigurationPath.Combine(tenantId, configurationPath)));
-        }
-        else
-        {
-            AddConfigurationObjectDefinitionTypes(services, typeof(ConfigurationObjectDefinition<>).MakeGenericType(type), configurationPath);
-        }
-    }
-
-    static void AddConfigurationObjectDefinitionTypes(IServiceCollection services, Type definitionType, string configurationPath)
-    {
-        var definition = Activator.CreateInstance(definitionType, configurationPath)!;
-        services.AddSingleton(definitionType, definition);
-        services.AddSingleton(typeof(IAmAConfigurationObjectDefinition), provider => provider.GetRequiredService(definitionType));
     }
 }
